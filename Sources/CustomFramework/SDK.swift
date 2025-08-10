05a59789b594f67a5d2f4cb0e1f8ff0174cc35d7
Rahul Rajput <rahulk@techaheadcorp.com>
2025-08-10 15:10:12 +0530

    sdk file added

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,15 @@
+//
+//  SDK.swift
+//  CustomFramework
+//
+//  Created by Rahul K on 10.08.25.
+//
+
+import Foundation
+
+public struct SDK {
+    
+    public static func doSomeWork() {
+        print("Doing something")
+    }
+}
