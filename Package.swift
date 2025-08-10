e91fccab75db569e7a940d1c095e35ad82e39ab0
Rahul Rajput <rahulk@techaheadcorp.com>
2025-08-10 15:29:24 +0530

    version : 0.0.2

--- Package.swift
+++ Package.swift
@@ -5,20 +5,19 @@ import PackageDescription
 
 let package = Package(
     name: "CustomFramework",
+    platforms: [
+        .macOS(.v12), .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "CustomFramework",
             targets: ["CustomFramework"]),
     ],
+    dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "CustomFramework"),
-        .testTarget(
-            name: "CustomFrameworkTests",
-            dependencies: ["CustomFramework"]
-        ),
+        .binaryTarget(name: "CustomFramework", path: "./Sources/CustomFramework.xcframework")
     ]
 )
